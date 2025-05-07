53cc0d2eec1df046eaad0a33ba004101d297a3bd
Euphoria2712 <byron0473@gmail.com>
2025-05-07 19:49:06 -0400

    1.1

--- Microservicios/Productos/src/main/java/com/example/Productos/controller/ControladorProducto.java
+++ Microservicios/Productos/src/main/java/com/example/Productos/controller/ControladorProducto.java
@@ -1,5 +1,33 @@
 package com.example.Productos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Productos.service.ServicePL;
+import com.example.Productos.Model.Producto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/productos")
 public class ControladorProducto {
+
+    @Autowired
+    private ServicePL servicePL;
+
+    @PostMapping
+    public Producto guardarProducto(@RequestBody Producto producto){
+        return servicePL.guardarProducto(producto);
+    }
+    
+    @GetMapping("/{id}")
+    public Producto buscarProductoPorId()
+    
+
+
 
 }
