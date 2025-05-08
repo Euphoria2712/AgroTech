a9d899b4d17e3175bafd026fb8a340cc7a67601c
Euphoria2712 <byron0473@gmail.com>
2025-05-07 21:42:46 -0400

    Me quiero matar

--- Microservicios/Productos/src/main/java/com/example/Productos/controller/ControladorProducto.java
+++ Microservicios/Productos/src/main/java/com/example/Productos/controller/ControladorProducto.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.Productos.service.ServicePL;
 import com.example.Productos.Model.Producto;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 
 @RestController
@@ -25,9 +29,25 @@ public class ControladorProducto {
     }
     
     @GetMapping("/{id}")
-    public Producto buscarProductoPorId()
+    public Producto buscarProductoPorId(@PathVariable Long id){
+        return servicePL.buscarProductoPorId(id);
+
+    }
+
+    @GetMapping
+    public List<Producto> buscarTodosLosProductos(){
+        return servicePL.buscarTodosLosProductos();
+    }
+
+    @PutMapping("/{id}")
+    public Producto modificarProducto(@PathVariable Long id, @RequestBody Producto producto){
+        return servicePL.modificarProducto(id, producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarProductoPorId(@PathVariable Long id){
+        servicePL.eliminarProductoPorId(id);
+    }
     
-
-
 
 }
