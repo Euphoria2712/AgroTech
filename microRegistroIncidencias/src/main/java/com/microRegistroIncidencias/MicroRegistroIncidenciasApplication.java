6e29c821bfef3e9be06a78fb1af16a9cad61177f
Simone-userr <si.hercules@duocuc.cl>
2025-05-22 01:07:41 -0400

    subidita del microservicios	Registro de incidencias (soporte técnico)
    	Registro de incidencias (soporte técnico)

---
+++ microRegistroIncidencias/src/main/java/com/microRegistroIncidencias/MicroRegistroIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.microRegistroIncidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRegistroIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroRegistroIncidenciasApplication.class, args);
+	}
+
+}
---
+++ microRegistroIncidencias/src/test/java/com/microRegistroIncidencias/MicroRegistroIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microRegistroIncidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRegistroIncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
