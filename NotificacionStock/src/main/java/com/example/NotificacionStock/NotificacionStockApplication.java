56fff7064ed8da016b3c82dd4fc2f17164da16e5
Euphoria2712 <164672585+Euphoria2712@users.noreply.github.com>
2025-05-25 01:43:55 -0400

    Microservicio Notificación de inventario

---
+++ NotificacionStock/src/main/java/com/example/NotificacionStock/NotificacionStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotificacionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacionStockApplication.class, args);
+	}
+
+}
