f67118eff4ea31dbd2bc0e1b02650ab199858ecc
Simone-userr <si.hercules@duocuc.cl>
2025-05-24 22:13:06 -0400

    Registro y autenticación de clientes

---
+++ Autenticación de clientes/src/test/java/com/AutenticacionDeCliente/Autenticacion/de/clientes/AutenticacionDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutenticacionDeCliente.Autenticacion.de.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
