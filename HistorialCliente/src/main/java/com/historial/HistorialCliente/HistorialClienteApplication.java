bc3f394c6fb8d460a17350b0059c05ee2a298e49
Simone-userr <si.hercules@duocuc.cl>
2025-05-25 16:12:07 -0400

    hisotrialClienteMicro

---
+++ HistorialCliente/src/main/java/com/historial/HistorialCliente/HistorialClienteApplication.java
@@ -0,0 +1,13 @@
+package com.historial.HistorialCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistorialClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistorialClienteApplication.class, args);
+	}
+
+}
---
+++ HistorialCliente/src/test/java/com/historial/HistorialCliente/HistorialClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historial.HistorialCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
