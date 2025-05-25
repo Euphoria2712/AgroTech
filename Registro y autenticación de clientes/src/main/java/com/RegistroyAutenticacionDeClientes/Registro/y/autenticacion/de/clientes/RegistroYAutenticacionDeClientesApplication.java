7d26f8928ef91c946ad7144d8a3717f3b82fca98
Simone-userr <si.hercules@duocuc.cl>
2025-05-24 22:00:38 -0400

    MicroservicioRegistroyAutenticacionDeCliente

---
+++ Registro y autenticación de clientes/src/main/java/com/RegistroyAutenticacionDeClientes/Registro/y/autenticacion/de/clientes/RegistroYAutenticacionDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.RegistroyAutenticacionDeClientes.Registro.y.autenticacion.de.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroYAutenticacionDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroYAutenticacionDeClientesApplication.class, args);
+	}
+
+}
---
+++ Registro y autenticación de clientes/src/test/java/com/RegistroyAutenticacionDeClientes/Registro/y/autenticacion/de/clientes/RegistroYAutenticacionDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RegistroyAutenticacionDeClientes.Registro.y.autenticacion.de.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroYAutenticacionDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
